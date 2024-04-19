387c6dc6ef12f509400b931a9252e656cdaa6996
Kostyable <kostyabl27@gmail.com>
2024-04-19 23:59:26 +0300

    feat: update MireaProject

---
+++ MireaProject/app/src/main/java/ru/mirea/blinnikovkm/mireaproject/ui/ForumWorker.java
@@ -0,0 +1,28 @@
+package ru.mirea.blinnikovkm.mireaproject.ui;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import java.util.concurrent.TimeUnit;
+
+public class ForumWorker extends Worker {
+    static final String TAG = "ForumWorker";
+    public ForumWorker(@NonNull Context context, @NonNull WorkerParameters params) {
+        super(context, params);
+    }
+    @Override
+    public Result doWork() {
+        Log.d(TAG, "doWork: start");
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Log.d(TAG, "doWork: end");
+        return Result.success();
+    }
+}
